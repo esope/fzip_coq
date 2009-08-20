b46d493550851a27d58b75e2ef0a6b660fd13aec
Benoît Montagu <benoit.montagu@m4x.org>
2009-08-20 17:20:59 +0200

    more efficient xcodec

--- xcodec/xcodec.ml
+++ xcodec/xcodec.ml
@@ -15,25 +15,22 @@
 (* The author would like to thank Nicolas Pouillard who wrote a piece     *)
 (* of code from which this file is inspired.                              *)
 
-let input = Stream.of_channel stdin
+let xcode = Char.code 'x'
+let id x = x
 
-let output_stream s =
-  Stream.iter (Printf.printf "%c") s; Printf.printf "%!"
+let rec encode input =
+  let n = Scanf.fscanf input "%c" Char.code in
+  let () = match n with
+  | n when n > 255 -> assert false
+  | n when n > 128 -> Printf.printf "x%x" n
+  | n when n = xcode -> Printf.printf "xx"
+  | _ -> Printf.printf "%c" (Char.chr n)
+  in encode input
 
-let stream_of_string s =
-  let n = String.length s in
-  let rec aux = function
-  | i when i = n -> [< >]
-  | i -> [< '(s.[i]) ; aux (i+1) >] in
-  aux 0
+let encode input =
+  try encode input
+  with End_of_file -> Printf.printf "%!"
 
-let rec encode = parser
-  | [< 'c when Char.code c > 255 ; _ >] -> assert false
-  | [< 'c when Char.code c > 128 ; t >] ->
-      [< ''x' ; stream_of_string (Printf.sprintf "%x" (Char.code c)) ; encode t >]
-  | [< ''x' ; t >] -> [< ''x' ; ''x' ; encode t >]
-  | [< 'c ; t >] -> [< 'c ; encode t >]
-  | [< >] -> [< >]
 
 let is_hex_digit = function
   | '0'..'9' | 'a'..'f' -> true
@@ -45,18 +42,23 @@ let string_of_four_chars c1 c2 c3 c4 =
 let read_hex_from_two_chars c1 c2 =
   int_of_string (string_of_four_chars '0' 'x' c1 c2)
 
-let rec decode = parser
-  | [< 'a when a = 'x' ; 'i ; s >] ->
+let rec decode input =
+  let c = Scanf.fscanf input "%c" id in
+  let () = match Char.code c with
+  | n when n = xcode ->
+      let i = Scanf.fscanf input "%c" id in
       if i = 'x'
-      then [< ''x' ; decode s >]
-      else (parser
-        | [< 'j ; s >] ->
-            if is_hex_digit i && is_hex_digit j
-            then [< '(Char.chr (read_hex_from_two_chars i j)) ; s >]
-            else [< ''x' ; 'i ; 'j ; s >]            
-        | [< >] -> assert false) (decode s)
-  | [< 'a ; s >] -> [< 'a ; decode s >]
-  | [< >] -> [< >]
+      then Printf.printf "x"
+      else let j = Scanf.fscanf input "%c" id in
+      if is_hex_digit i && is_hex_digit j
+      then Printf.printf "%c" (Char.chr (read_hex_from_two_chars i j))
+      else Printf.printf "x%c%c" i j
+  | _ -> Printf.printf "%c" c
+  in decode input
+
+let decode input =
+  try decode input
+  with End_of_file -> Printf.printf "%!"
 
 let exit_usage n =
   Printf.eprintf "Usage: xcodec [encode|decode]\n%!";
@@ -67,8 +69,8 @@ let () = (* main *)
   then exit_usage 1
   else begin
     match Sys.argv.(1) with
-    | "encode" -> output_stream (encode input)
-    | "decode" -> output_stream (decode input)
+    | "encode" -> encode stdin
+    | "decode" -> decode stdin
     | _ -> exit_usage 1
   end ;
   exit 0
