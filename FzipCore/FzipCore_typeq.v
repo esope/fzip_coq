df8db2dc3f93ca3a2923f4ce4445133fc6740367
Benoît Montagu <benoit.montagu@m4x.org>
2010-04-05 16:59:08 +0200

    inversion lemmas for typeq

--- FzipCore/FzipCore_typeq.v
+++ FzipCore/FzipCore_typeq.v
@@ -492,3 +492,454 @@ Case "exists". pick fresh b and apply wftypeq_exists.
 rewrite_env (([(b, U)] ++ Γ₁) ++ [(a, U)] ++ Γ₂ ++ Γ₃). auto.
 Case "trans". eauto.
 Qed.
+
+Lemma wftypeq_unfold_eq : forall Γ τ a τ',
+  wftyp Γ τ → binds a (Eq τ') Γ →
+  wftypeq Γ τ (tsubst_typ τ' a τ).
+Proof.
+intros Γ τ a τ' H H0. induction H; simpl; auto.
+Case "var". unfold typvar in *; destruct (a0 == a); subst; auto.
+Case "forall". pick fresh b and apply wftypeq_forall.
+assert (b ≠ a) by fsetdec.
+replace (typ_var_f b) with (tsubst_typ τ' a (typ_var_f b)).
+rewrite <- tsubst_typ_open_typ_wrt_typ.
+simpl; unfold typvar in *; destruct (b == a); subst; simpl_env; auto; try congruence.
+apply wftyp_regular with (Γ := b ~ U ++ G).
+  apply wfenv_wftyp_Eq2 with (x := a); auto. eapply wftyp_wfenv; eauto.
+simpl; unfold typvar in *; destruct (b == a); subst; auto; try congruence.
+Case "exists". pick fresh b and apply wftypeq_exists.
+assert (b ≠ a) by fsetdec.
+replace (typ_var_f b) with (tsubst_typ τ' a (typ_var_f b)).
+rewrite <- tsubst_typ_open_typ_wrt_typ.
+simpl; unfold typvar in *; destruct (b == a); subst; simpl_env; auto; try congruence.
+apply wftyp_regular with (Γ := b ~ U ++ G).
+  apply wfenv_wftyp_Eq2 with (x := a); auto. eapply wftyp_wfenv; eauto.
+simpl; unfold typvar in *; destruct (b == a); subst; auto; try congruence.
+Qed.
+
+Inductive typeq_unfold : typing_env → typ → typ → Prop :=
+| typeq_unfold_var : forall Γ a,
+    wfenv Γ → (binds a U Γ ∨ binds a E Γ) →
+    typeq_unfold Γ (typ_var_f a) (typ_var_f a)
+| typeq_unfold_eq : forall Γ a τ τ',
+    binds a (Eq τ) Γ → typeq_unfold Γ τ τ' →
+    typeq_unfold Γ (typ_var_f a) τ'
+| typeq_unfold_arrow : forall Γ τ₁ τ₂ τ₁' τ₂',
+    typeq_unfold Γ τ₁ τ₁' → typeq_unfold Γ τ₂ τ₂' →
+    typeq_unfold Γ (typ_arrow τ₁ τ₂) (typ_arrow τ₁' τ₂')
+| typeq_unfold_prod : forall Γ τ₁ τ₂ τ₁' τ₂',
+    typeq_unfold Γ τ₁ τ₁' → typeq_unfold Γ τ₂ τ₂' →
+    typeq_unfold Γ (typ_prod τ₁ τ₂) (typ_prod τ₁' τ₂')
+| typeq_unfold_forall : forall L Γ τ τ',
+    (forall a, a ∉ L →
+      typeq_unfold (a ~ U ++ Γ)
+                   (open_typ_wrt_typ τ  (typ_var_f a))
+                   (open_typ_wrt_typ τ' (typ_var_f a))) →
+    typeq_unfold Γ (typ_forall τ) (typ_forall τ')
+| typeq_unfold_exists : forall L Γ τ τ',
+    (forall a, a ∉ L →
+      typeq_unfold (a ~ U ++ Γ)
+                   (open_typ_wrt_typ τ  (typ_var_f a))
+                   (open_typ_wrt_typ τ' (typ_var_f a))) →
+    typeq_unfold Γ (typ_exists τ) (typ_exists τ').
+Hint Constructors typeq_unfold.
+
+Lemma typeq_unfold_wfenv : forall Γ τ τ',
+  typeq_unfold Γ τ τ' → wfenv Γ.
+Proof.
+intros Γ τ τ' H. induction H; auto.
+Case "forall". pick fresh a; apply wfenv_strip with (Γ' := a ~ U); eauto.
+Case "exists". pick fresh a; apply wfenv_strip with (Γ' := a ~ U); eauto.
+Qed.
+
+Lemma typeq_unfold_weakening : forall Γ₁ Γ₂ Γ₃ τ τ',
+  typeq_unfold (Γ₁ ++ Γ₃) τ τ' →
+  wfenv (Γ₁ ++ Γ₂ ++ Γ₃) →
+  typeq_unfold (Γ₁ ++ Γ₂ ++ Γ₃) τ τ'.
+Proof.
+intros Γ₁ Γ₂ Γ₃ τ τ' H H0. dependent induction H; eauto.
+Case "var". constructor; auto.
+destruct H0; analyze_binds H0.
+Case "forall". pick fresh b and apply typeq_unfold_forall; intros.
+rewrite_env ((b ~ U ++ Γ₁) ++ Γ₂ ++ Γ₃). apply H0; simpl_env; auto.
+Case "exists". pick fresh b and apply typeq_unfold_exists; intros.
+rewrite_env ((b ~ U ++ Γ₁) ++ Γ₂ ++ Γ₃). apply H0; simpl_env; auto.
+Qed.
+
+Lemma typeq_unfold_unique : forall Γ τ τ₁ τ₂,
+  typeq_unfold Γ τ τ₁ → typeq_unfold Γ τ τ₂ → τ₁ = τ₂.
+Proof.
+intros Γ τ τ₁ τ₂ H H0. generalize dependent τ₂.
+induction H; intros τ₀ H₀; inversion H₀; subst; auto.
+Case "var eq".
+elimtype False. destruct H0.
+assert (Eq τ = U). eapply binds_unique; eauto with lngen.
+  congruence.
+assert (Eq τ = E). eapply binds_unique; eauto with lngen.
+  congruence.
+Case "eq var".
+elimtype False. destruct H4.
+assert (Eq τ = U). eapply binds_unique; eauto with lngen.
+  congruence.
+assert (Eq τ = E). eapply binds_unique; eauto with lngen.
+  congruence.
+Case "eq eq".
+assert (Eq τ = Eq τ0). eapply binds_unique; eauto using typeq_unfold_wfenv with lngen.
+replace τ0 with τ in * by congruence. auto.
+Case "arrow". f_equal; auto.
+Case "prod". f_equal; auto.
+Case "forall". f_equal. pick fresh a.
+apply open_typ_wrt_typ_inj with (a1 := a); auto.
+Case "exists". f_equal. pick fresh a.
+apply open_typ_wrt_typ_inj with (a1 := a); auto.
+Qed.
+
+Lemma typeq_unfold_wftypeq : forall Γ τ τ',
+  typeq_unfold Γ τ τ' → wftypeq Γ τ τ'.
+Proof.
+intros Γ τ τ' H. induction H; eauto.
+Case "var". constructor; auto. tauto.
+Case "eq". assert (wfenv Γ) by eauto using typeq_unfold_wfenv.
+eauto.
+Qed.
+
+Lemma typeq_unfold_renameU : forall Γ₁ a Γ₂ τ τ' b,
+  typeq_unfold (Γ₁ ++ a ~ U ++ Γ₂) τ τ' → b ∉ dom (Γ₁ ++ Γ₂) →
+  typeq_unfold
+    (env_map (tsubst_typ (typ_var_f b) a) Γ₁ ++ b ~ U ++ Γ₂)
+    (tsubst_typ (typ_var_f b) a τ)
+    (tsubst_typ (typ_var_f b) a τ').
+Proof.
+intros Γ₁ a Γ₂ τ τ' b H H0. dependent induction H; simpl; simpl_env; auto.
+Case "var". unfold typvar; destruct (a == a0); subst.
+SCase "a = a0". constructor. auto using wfenv_renameU.
+destruct H0; analyze_binds H0.
+SCase "a ≠ a0". constructor. auto using wfenv_renameU.
+destruct H0; analyze_binds H0.
+SSCase "binds a U Γ₁".
+replace (@U typ) with (tag_map (tsubst_typ (typ_var_f b) a0) U) by reflexivity.
+unfold env_map. auto.
+SSCase "binds a E Γ₁".
+replace (@E typ) with (tag_map (tsubst_typ (typ_var_f b) a0) E) by reflexivity.
+unfold env_map. auto.
+Case "eq". unfold typvar; destruct (a == a0); subst.
+SCase "a = a0 (absurd)". elimtype False.
+analyze_binds_uniq H. eauto using typeq_unfold_wfenv with lngen.
+SCase "a ≠ a0". analyze_binds H.
+SSCase "a binds in Γ₁". apply typeq_unfold_eq with (τ := (tsubst_typ (typ_var_f b) a0 τ)); auto.
+replace (Eq (tsubst_typ (typ_var_f b) a0 τ)) with
+(tag_map (tsubst_typ (typ_var_f b) a0) (Eq τ)) by reflexivity.
+unfold env_map; auto.
+SSCase "a binds in Γ₂". apply typeq_unfold_eq with (τ := τ); auto.
+rewrite <- tsubst_typ_fresh_eq with (t2 := τ) (a1 := a0) (t1 := typ_var_f b); auto.
+assert (ftv_typ τ [<=] dom Γ₂). apply wftyp_ftv. eapply wfenv_wftyp_Eq2; eauto. apply wfenv_strip with (Γ' := Γ₁ ++ a0 ~ U). eapply typeq_unfold_wfenv; simpl_env; eauto.
+assert (uniq (Γ₁ ++ [(a0, U)] ++ Γ₂)). eauto using typeq_unfold_wfenv with lngen.
+destruct_uniq; fsetdec.
+Case "forall". pick fresh c and apply typeq_unfold_forall; intros.
+rewrite_env (env_map (tsubst_typ (typ_var_f b) a) (c ~ U ++ Γ₁) ++ [(b, U)] ++ Γ₂).
+repeat rewrite tsubst_typ_open_typ_wrt_typ_var; auto.
+Case "exists". pick fresh c and apply typeq_unfold_exists; intros.
+rewrite_env (env_map (tsubst_typ (typ_var_f b) a) (c ~ U ++ Γ₁) ++ [(b, U)] ++ Γ₂).
+repeat rewrite tsubst_typ_open_typ_wrt_typ_var; auto.
+Qed.
+
+Lemma typeq_unfold_defined_aux :
+  (forall Γ, wfenv Γ → forall Γ₁ a τ Γ₂,
+    Γ = Γ₁ ++ a ~ Eq τ ++ Γ₂ → exists τ', typeq_unfold Γ₂ τ τ')
+  ∧ (forall Γ τ, wftyp Γ τ →
+    (forall Γ₁ a τ Γ₂,
+      Γ = Γ₁ ++ a ~ Eq τ ++ Γ₂ → exists τ', typeq_unfold Γ₂ τ τ')
+    ∧ exists τ', typeq_unfold Γ τ τ').
+Proof.
+apply wfenv_wftyp_mut_ind; intros.
+Case "nil". destruct Γ₁; inversion H.
+Case "T". destruct Γ₁; inversion H0; subst.
+destruct H. destruct (H Γ₁ a τ Γ₂). simpl_env; auto. eauto.
+Case "U". destruct Γ₁; inversion H0; subst.
+destruct (H Γ₁ a0 τ Γ₂). simpl_env; auto. eauto.
+Case "E". destruct Γ₁; inversion H0; subst.
+destruct (H Γ₁ a0 τ Γ₂). simpl_env; auto. eauto.
+Case "Eq". destruct H. destruct Γ₁; inversion H0; subst.
+destruct H1; eauto.
+destruct (H Γ₁ a0 τ Γ₂). simpl_env; auto. eauto.
+Case "var". split; intros; eauto.
+destruct o; eauto. destruct H0; eauto.
+destruct H0.
+destruct (binds_decomp G a (Eq x)) as [? [? ?]]; auto; subst.
+destruct (H x0 a x x1); auto.
+exists x2. apply typeq_unfold_eq with (τ := x); auto.
+rewrite_env (nil ++ (x0 ++ a ~ Eq x) ++ x1).
+apply typeq_unfold_weakening; simpl_env; auto.
+Case "arrow". destruct H. destruct H0. split; intros; subst.
+edestruct H; eauto.
+destruct H1; destruct H2; eauto.
+Case "prod". destruct H. destruct H0. split; intros; subst.
+edestruct H; eauto.
+destruct H1; destruct H2; eauto.
+Case "forall". pick fresh b. edestruct H; eauto. clear H. split; intros; subst.
+destruct (H0 (b ~ U ++ Γ₁) a τ Γ₂); simpl_env; eauto.
+destruct H1.
+rewrite <- open_typ_wrt_typ_close_typ_wrt_typ with (t1 := x) (a1 := b) in H.
+exists (typ_forall (close_typ_wrt_typ b x)).
+apply typeq_unfold_forall with (L := L ∪ dom G); intros.
+rewrite_env (env_map (tsubst_typ (typ_var_f a) b) nil ++ a ~ U ++ G).
+replace (open_typ_wrt_typ t (typ_var_f a)) with (tsubst_typ (typ_var_f a) b (open_typ_wrt_typ t (typ_var_f b))).
+replace (open_typ_wrt_typ (close_typ_wrt_typ b x) (typ_var_f a)) with (tsubst_typ (typ_var_f a) b (open_typ_wrt_typ (close_typ_wrt_typ b x) (typ_var_f b))).
+apply typeq_unfold_renameU; simpl_env; auto.
+autorewrite with lngen. apply tsubst_typ_spec.
+rewrite tsubst_typ_open_typ_wrt_typ; auto. simpl.
+unfold typvar; destruct (b == b); try congruence.
+rewrite tsubst_typ_fresh_eq; auto.
+Case "exists". pick fresh b. edestruct H; eauto. clear H. split; intros; subst.
+destruct (H0 (b ~ U ++ Γ₁) a τ Γ₂); simpl_env; eauto.
+destruct H1.
+rewrite <- open_typ_wrt_typ_close_typ_wrt_typ with (t1 := x) (a1 := b) in H.
+exists (typ_exists (close_typ_wrt_typ b x)).
+apply typeq_unfold_exists with (L := L ∪ dom G); intros.
+rewrite_env (env_map (tsubst_typ (typ_var_f a) b) nil ++ a ~ U ++ G).
+replace (open_typ_wrt_typ t (typ_var_f a)) with (tsubst_typ (typ_var_f a) b (open_typ_wrt_typ t (typ_var_f b))).
+replace (open_typ_wrt_typ (close_typ_wrt_typ b x) (typ_var_f a)) with (tsubst_typ (typ_var_f a) b (open_typ_wrt_typ (close_typ_wrt_typ b x) (typ_var_f b))).
+apply typeq_unfold_renameU; simpl_env; auto.
+autorewrite with lngen. apply tsubst_typ_spec.
+rewrite tsubst_typ_open_typ_wrt_typ; auto. simpl.
+unfold typvar; destruct (b == b); try congruence.
+rewrite tsubst_typ_fresh_eq; auto.
+Qed.
+
+Lemma typeq_unfold_defined : forall Γ τ,
+  wftyp Γ τ → exists τ', typeq_unfold Γ τ τ'.
+Proof.
+intros Γ τ H. destruct typeq_unfold_defined_aux as [_ H0].
+edestruct H0 as [_ ?]; eauto.
+Qed.
+
+Definition algtypeq Γ τ₁ τ₂ := exists τ₀,
+  typeq_unfold Γ τ₁ τ₀ ∧ typeq_unfold Γ τ₂ τ₀.
+Hint Unfold algtypeq.
+
+Lemma algtypeq_refl : forall Γ τ, wftyp Γ τ → algtypeq Γ τ τ.
+Proof.
+intros Γ τ H. apply typeq_unfold_defined in H. destruct H; eauto.
+Qed.
+
+Lemma algtypeq_sym : forall Γ τ₁ τ₂,
+  algtypeq Γ τ₁ τ₂ → algtypeq Γ τ₂ τ₁.
+Proof.
+intros Γ τ₁ τ₂ H. destruct H as [? [? ?]]; eauto.
+Qed.
+
+Lemma algtypeq_trans : forall Γ τ₁ τ₂ τ₃,
+  algtypeq Γ τ₁ τ₂ → algtypeq Γ τ₂ τ₃ → algtypeq Γ τ₁ τ₃.
+Proof.
+intros Γ τ₁ τ₂ τ₃ H H0.
+destruct H as [τ₁₂ [? ?]]. destruct H0 as [τ₂₃ [? ?]].
+assert (τ₁₂ = τ₂₃) by eauto using typeq_unfold_unique. subst.
+eauto.
+Qed.
+
+Lemma algtypeq_wftypeq : forall Γ τ₁ τ₂,
+  algtypeq Γ τ₁ τ₂ → wftypeq Γ τ₁ τ₂.
+Proof.
+intros Γ τ₁ τ₂ H. destruct H as [τ₃ [? ?]].
+eauto using typeq_unfold_wftypeq.
+Qed.
+
+Lemma wftypeq_algtypeq : forall Γ τ₁ τ₂,
+  wftypeq Γ τ₁ τ₂ → algtypeq Γ τ₁ τ₂.
+Proof.
+intros Γ τ₁ τ₂ H. induction H.
+Case "var". destruct H. eauto 8. destruct H. eauto 8.
+destruct H. assert (exists τ, typeq_unfold G x τ) as [? ?].
+apply typeq_unfold_defined. eapply wfenv_wftyp_Eq2; eauto.
+eauto 6.
+Case "eq". assert (exists τ, typeq_unfold G t τ) as [? ?].
+apply typeq_unfold_defined. eapply wfenv_wftyp_Eq2; eauto.
+eauto 6.
+Case "arrow". destruct IHwftypeq1 as [? [? ?]]; destruct IHwftypeq2 as [? [? ?]].
+eauto 6.
+Case "prod". destruct IHwftypeq1 as [? [? ?]]; destruct IHwftypeq2 as [? [? ?]].
+eauto 6.
+Case "forall". pick fresh a.
+edestruct H0 as [? [? ?]]; eauto. clear H0.
+rewrite <- open_typ_wrt_typ_close_typ_wrt_typ with (a1 := a) (t1 := x) in H1.
+rewrite <- open_typ_wrt_typ_close_typ_wrt_typ with (a1 := a) (t1 := x) in H2.
+exists (typ_forall (close_typ_wrt_typ a x)). split.
+SCase "left part". clear H2.
+apply typeq_unfold_forall with (L := L ∪ {{a}} ∪ ftv_typ t1 ∪ ftv_typ x ∪ dom G); intros.
+rewrite_env (nil ++ a ~ U ++ G) in H1.
+apply typeq_unfold_renameU with (b := a0) in H1; auto.
+rewrite tsubst_typ_open_typ_wrt_typ in H1; auto.
+simpl in H1.
+unfold typvar in *; destruct (a == a); try congruence.
+rewrite tsubst_typ_open_typ_wrt_typ in H1; auto.
+simpl in H1.
+unfold typvar in *; destruct (a == a); try congruence.
+rewrite tsubst_typ_fresh_eq with (t2 := t1) in H1; auto.
+rewrite tsubst_typ_fresh_eq with (t2 := close_typ_wrt_typ a x) in H1; auto.
+autorewrite with lngen. fsetdec.
+SCase "right part". clear H1.
+apply typeq_unfold_forall with (L := L ∪ {{a}} ∪ ftv_typ t2 ∪ ftv_typ x ∪ dom G); intros.
+rewrite_env (nil ++ a ~ U ++ G) in H2.
+apply typeq_unfold_renameU with (b := a0) in H2; auto.
+rewrite tsubst_typ_open_typ_wrt_typ in H2; auto.
+simpl in H2.
+unfold typvar in *; destruct (a == a); try congruence.
+rewrite tsubst_typ_open_typ_wrt_typ in H2; auto.
+simpl in H2.
+unfold typvar in *; destruct (a == a); try congruence.
+rewrite tsubst_typ_fresh_eq with (t2 := t2) in H2; auto.
+rewrite tsubst_typ_fresh_eq with (t2 := close_typ_wrt_typ a x) in H2; auto.
+autorewrite with lngen. fsetdec.
+Case "exists". pick fresh a.
+edestruct H0 as [? [? ?]]; eauto. clear H0.
+rewrite <- open_typ_wrt_typ_close_typ_wrt_typ with (a1 := a) (t1 := x) in H1.
+rewrite <- open_typ_wrt_typ_close_typ_wrt_typ with (a1 := a) (t1 := x) in H2.
+exists (typ_exists (close_typ_wrt_typ a x)). split.
+SCase "left part". clear H2.
+apply typeq_unfold_exists with (L := L ∪ {{a}} ∪ ftv_typ t1 ∪ ftv_typ x ∪ dom G); intros.
+rewrite_env (nil ++ a ~ U ++ G) in H1.
+apply typeq_unfold_renameU with (b := a0) in H1; auto.
+rewrite tsubst_typ_open_typ_wrt_typ in H1; auto.
+simpl in H1.
+unfold typvar in *; destruct (a == a); try congruence.
+rewrite tsubst_typ_open_typ_wrt_typ in H1; auto.
+simpl in H1.
+unfold typvar in *; destruct (a == a); try congruence.
+rewrite tsubst_typ_fresh_eq with (t2 := t1) in H1; auto.
+rewrite tsubst_typ_fresh_eq with (t2 := close_typ_wrt_typ a x) in H1; auto.
+autorewrite with lngen. fsetdec.
+SCase "right part". clear H1.
+apply typeq_unfold_exists with (L := L ∪ {{a}} ∪ ftv_typ t2 ∪ ftv_typ x ∪ dom G); intros.
+rewrite_env (nil ++ a ~ U ++ G) in H2.
+apply typeq_unfold_renameU with (b := a0) in H2; auto.
+rewrite tsubst_typ_open_typ_wrt_typ in H2; auto.
+simpl in H2.
+unfold typvar in *; destruct (a == a); try congruence.
+rewrite tsubst_typ_open_typ_wrt_typ in H2; auto.
+simpl in H2.
+unfold typvar in *; destruct (a == a); try congruence.
+rewrite tsubst_typ_fresh_eq with (t2 := t2) in H2; auto.
+rewrite tsubst_typ_fresh_eq with (t2 := close_typ_wrt_typ a x) in H2; auto.
+autorewrite with lngen. fsetdec.
+Case "sym". auto using algtypeq_sym.
+Case "trans". eauto using algtypeq_trans.
+Qed.
+
+Lemma wftypeq_arrow_inv1 : forall Γ τ₁ τ₂ τ₁' τ₂',
+  wftypeq Γ (typ_arrow τ₁ τ₂) (typ_arrow τ₁' τ₂') →
+  wftypeq Γ τ₁ τ₁'.
+Proof.
+intros Γ τ₁ τ₂ τ₁' τ₂' H.
+apply wftypeq_algtypeq in H. destruct H as [? [? ?]].
+inversion H; subst. inversion H0; subst.
+apply algtypeq_wftypeq. eauto.
+Qed.
+
+Lemma wftypeq_arrow_inv2 : forall Γ τ₁ τ₂ τ₁' τ₂',
+  wftypeq Γ (typ_arrow τ₁ τ₂) (typ_arrow τ₁' τ₂') →
+  wftypeq Γ τ₂ τ₂'.
+Proof.
+intros Γ τ₁ τ₂ τ₁' τ₂' H.
+apply wftypeq_algtypeq in H. destruct H as [? [? ?]].
+inversion H; subst. inversion H0; subst.
+apply algtypeq_wftypeq. eauto.
+Qed.
+
+Lemma wftypeq_prod_inv1 : forall Γ τ₁ τ₂ τ₁' τ₂',
+  wftypeq Γ (typ_prod τ₁ τ₂) (typ_prod τ₁' τ₂') →
+  wftypeq Γ τ₁ τ₁'.
+Proof.
+intros Γ τ₁ τ₂ τ₁' τ₂' H.
+apply wftypeq_algtypeq in H. destruct H as [? [? ?]].
+inversion H; subst. inversion H0; subst.
+apply algtypeq_wftypeq. eauto.
+Qed.
+
+Lemma wftypeq_prod_inv2 : forall Γ τ₁ τ₂ τ₁' τ₂',
+  wftypeq Γ (typ_prod τ₁ τ₂) (typ_prod τ₁' τ₂') →
+  wftypeq Γ τ₂ τ₂'.
+Proof.
+intros Γ τ₁ τ₂ τ₁' τ₂' H.
+apply wftypeq_algtypeq in H. destruct H as [? [? ?]].
+inversion H; subst. inversion H0; subst.
+apply algtypeq_wftypeq. eauto.
+Qed.
+
+Lemma wftypeq_forall_inv : forall Γ τ τ' a,
+  wftypeq Γ (typ_forall τ) (typ_forall τ') →
+  a ∉ dom Γ →
+  wftypeq (a ~ U ++ Γ)
+          (open_typ_wrt_typ τ (typ_var_f a))
+          (open_typ_wrt_typ τ' (typ_var_f a)).
+Proof.
+intros Γ τ τ' a H Ha.
+apply wftypeq_algtypeq in H. destruct H as [? [? ?]].
+inversion H; subst. inversion H0; subst. clear H H0.
+apply algtypeq_wftypeq.
+pick fresh b.
+assert (typeq_unfold ([(b, U)] ++ Γ)
+  (open_typ_wrt_typ τ (typ_var_f b))
+  (open_typ_wrt_typ τ'0 (typ_var_f b))) by auto; clear H3.
+rewrite_env (nil ++ b ~ U ++ Γ) in H.
+apply typeq_unfold_renameU with (b := a) in H; auto.
+rewrite tsubst_typ_open_typ_wrt_typ in H; auto.
+simpl in H. unfold typvar in *; destruct (b == b); try congruence.
+rewrite tsubst_typ_open_typ_wrt_typ in H; auto.
+simpl in H. unfold typvar in *; destruct (b == b); try congruence.
+rewrite tsubst_typ_fresh_eq in H; auto.
+rewrite tsubst_typ_fresh_eq in H; auto.
+simpl_env in H.
+assert (typeq_unfold ([(b, U)] ++ Γ)
+  (open_typ_wrt_typ τ' (typ_var_f b))
+  (open_typ_wrt_typ τ'0 (typ_var_f b))) by auto; clear H5.
+rewrite_env (nil ++ b ~ U ++ Γ) in H0.
+apply typeq_unfold_renameU with (b := a) in H0; auto.
+rewrite tsubst_typ_open_typ_wrt_typ in H0; auto.
+simpl in H0. unfold typvar in *; destruct (b == b); try congruence.
+rewrite tsubst_typ_open_typ_wrt_typ in H0; auto.
+simpl in H0. unfold typvar in *; destruct (b == b); try congruence.
+rewrite tsubst_typ_fresh_eq in H0; auto.
+rewrite tsubst_typ_fresh_eq in H0; auto.
+simpl_env in H0.
+eauto.
+Qed.
+
+Lemma wftypeq_exists_inv : forall Γ τ τ' a,
+  wftypeq Γ (typ_exists τ) (typ_exists τ') →
+  a ∉ dom Γ →
+  wftypeq (a ~ U ++ Γ)
+          (open_typ_wrt_typ τ (typ_var_f a))
+          (open_typ_wrt_typ τ' (typ_var_f a)).
+Proof.
+intros Γ τ τ' a H Ha.
+apply wftypeq_algtypeq in H. destruct H as [? [? ?]].
+inversion H; subst. inversion H0; subst. clear H H0.
+apply algtypeq_wftypeq.
+pick fresh b.
+assert (typeq_unfold ([(b, U)] ++ Γ)
+  (open_typ_wrt_typ τ (typ_var_f b))
+  (open_typ_wrt_typ τ'0 (typ_var_f b))) by auto; clear H3.
+rewrite_env (nil ++ b ~ U ++ Γ) in H.
+apply typeq_unfold_renameU with (b := a) in H; auto.
+rewrite tsubst_typ_open_typ_wrt_typ in H; auto.
+simpl in H. unfold typvar in *; destruct (b == b); try congruence.
+rewrite tsubst_typ_open_typ_wrt_typ in H; auto.
+simpl in H. unfold typvar in *; destruct (b == b); try congruence.
+rewrite tsubst_typ_fresh_eq in H; auto.
+rewrite tsubst_typ_fresh_eq in H; auto.
+simpl_env in H.
+assert (typeq_unfold ([(b, U)] ++ Γ)
+  (open_typ_wrt_typ τ' (typ_var_f b))
+  (open_typ_wrt_typ τ'0 (typ_var_f b))) by auto; clear H5.
+rewrite_env (nil ++ b ~ U ++ Γ) in H0.
+apply typeq_unfold_renameU with (b := a) in H0; auto.
+rewrite tsubst_typ_open_typ_wrt_typ in H0; auto.
+simpl in H0. unfold typvar in *; destruct (b == b); try congruence.
+rewrite tsubst_typ_open_typ_wrt_typ in H0; auto.
+simpl in H0. unfold typvar in *; destruct (b == b); try congruence.
+rewrite tsubst_typ_fresh_eq in H0; auto.
+rewrite tsubst_typ_fresh_eq in H0; auto.
+simpl_env in H0.
+eauto.
+Qed.
+
