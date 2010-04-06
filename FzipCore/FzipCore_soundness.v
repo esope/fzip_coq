a8ceeba05f59dc52ffa46c1074e571351487bb61
Benoît Montagu <benoit.montagu@m4x.org>
2010-04-06 19:07:29 +0200

    new cases for SR0

--- FzipCore/FzipCore_soundness.v
+++ FzipCore/FzipCore_soundness.v
@@ -130,53 +130,260 @@ apply binds_decomp in H3. destruct H3 as [? [? ?]]; subst.
 apply binds_decomp in H4. destruct H4 as [? [? ?]]; subst.
 pick fresh a and apply wfterm_sigma.
 assert (uniq (x1 ++ [(b, E)] ++ x2)) by eauto with lngen. solve_uniq.
+assert (lc_typ t).
+  assert (lc_term (term_sigma (typ_var_f b) t e1))
+    by eauto with lngen. inversion H3; subst; auto.
 replace (open_term_wrt_typ (term_app e1 e2') (typ_var_f a)) with
   (term_app (open_term_wrt_typ e1 (typ_var_f a)) (open_term_wrt_typ e2' (typ_var_f a))) by reflexivity.
 rewrite H2; auto.
-apply zip_remove_EU in H5.
 apply wfterm_app with
   (G1 := a ~ Eq t ++ G3 ++ G0)
   (G2 := a ~ Eq t ++ x ++ x0) (t2 := tsubst_typ (typ_var_f a) b t2).
-assert (lc_term (term_sigma (typ_var_f b) t e1)) by eauto with lngen. inversion H3; subst.
-constructor; auto.
+constructor; eauto using zip_remove_EU.
 apply H12; auto.
 rewrite_env (env_map (tsubst_typ (typ_var_f a) b) nil ++ a ~ Eq t ++ x ++ x0).
 apply wfterm_instantiate.
 apply wfterm_renameU; auto.
 apply wfterm_lowerU; auto.
-intro H3. apply ftv_env_binds in H3. destruct H3 as [? [? [? [? | ?]]]].
-  assert (binds x3 (T x4) (x ++ x0)) by auto.
-    eapply zip_binds_T23 in H6; eauto. eapply zip_binds_T31 in H6; eauto.
+intro H4. apply ftv_env_binds in H4. destruct H4 as [? [? [? [? | ?]]]].
+  assert (binds x3 (T x4) (x ++ b ~ U ++ x0)) by auto.
+    eapply zip_binds_T23 in H8; eauto. eapply zip_binds_T31 in H8; eauto.
     assert (ftv_typ x4 [<=] dom (G3 ++ G0)). apply wftyp_ftv.
     apply wfenv_wftyp_T2 with (x := x3); auto.
     apply wfenv_strip with (Γ' := a ~ Eq t). eapply wfterm_wfenv. eauto with fzip.
+    analyze_binds H8.
     clear Fr. assert (b ∉ ftv_typ x4) by fsetdec. contradiction.
-  assert (binds x3 (Eq x4) (x ++ x0)) by auto.
-    eapply zip_binds_Eq23 in H6; eauto. eapply zip_binds_Eq31 in H6; eauto.
+  assert (binds x3 (Eq x4) (x ++ b ~ U ++ x0)) by auto.
+    eapply zip_binds_Eq23 in H8; eauto. eapply zip_binds_Eq31 in H8; eauto.
     assert (ftv_typ x4 [<=] dom (G3 ++ G0)). apply wftyp_ftv.
     apply wfenv_wftyp_Eq2 with (x := x3); auto.
     apply wfenv_strip with (Γ' := a ~ Eq t). eapply wfterm_wfenv; eauto.
+    analyze_binds H8.
     clear Fr. assert (b ∉ ftv_typ x4) by fsetdec. contradiction.
-
-ICI
-
-eapply wftyp_zip12; eauto.
-  apply wfenv_wftyp_Eq3 with (x := a). eapply wfterm_wfenv; eauto.
-
-  apply wfenv_strip with (Γ' := a ~ Eq t). eapply wfterm_wfenv; eauto.
-
-
-Case "sigma_appR".
-Case "sigma_pairL".
-Case "sigma_pairR".
-Case "sigma_fset".
-Case "sigma_snd".
-Case "sigma_inst".
+apply zip_remove_EU in H5. eapply wftyp_zip12; eauto. apply wfenv_wftyp_Eq3 with (x := a).
+eapply wfterm_wfenv; eauto.
+eapply wfenv_zip12; eauto. apply wfenv_strip with (Γ' := a ~ Eq t).
+eapply wfterm_wfenv; eauto.
+apply zip_remove_EU in H5.
+intros.
+assert (a0 ∈ dom (G3 ++ G0)).
+assert (ftv_typ t [<=] dom (G3 ++ G0)). apply wftyp_ftv. apply wfenv_wftyp_Eq3 with (x := a). eapply wfterm_wfenv; eauto. auto.
+apply binds_In_inv in H6. destruct H6. destruct x3.
+eapply zip_binds_T12 in H6; eauto. intro.
+  assert (E = T t0). eapply binds_unique; eauto with lngen. congruence.
+eapply zip_binds_U12 in H6; eauto. intro.
+  assert (@E typ = U). eapply binds_unique; eauto with lngen. congruence.
+eapply zip_binds_E12 in H6; eauto. intro.
+  assert (@E typ = U). eapply binds_unique; eauto with lngen. congruence.
+eapply zip_binds_Eq12 in H6; eauto. intro.
+  assert (@E typ = Eq t0). eapply binds_unique; eauto with lngen. congruence.
+Case "sigma_appR". inversion H9; subst.
+assert (binds b E Γ) by eauto with fzip.
+assert (b ∉ dom G1).
+  eapply zip_binds_E3_inv in H3; eauto. destruct H3 as [[? ?] | [? ?]]; auto.
+  analyze_binds_uniq H4; eauto with lngen.
+apply binds_decomp in H3. destruct H3 as [? [? ?]]; subst.
+pick fresh a and apply wfterm_sigma.
+assert (uniq (x ++ [(b, E)] ++ x0)) by eauto with lngen. solve_uniq.
+assert (lc_typ t).
+  assert (lc_term (term_sigma (typ_var_f b) t e2))
+    by eauto with lngen. inversion H3; subst; auto.
+replace (open_term_wrt_typ (term_app e1' e2) (typ_var_f a)) with
+  (term_app (open_term_wrt_typ e1' (typ_var_f a)) (open_term_wrt_typ e2 (typ_var_f a))) by reflexivity.
+rewrite H2; auto.
+apply wfterm_app with
+  (G1 := a ~ Eq t ++ G1)
+  (G2 := a ~ Eq t ++ G3 ++ G0) (t2 := tsubst_typ (typ_var_f a) b t2); eauto.
+constructor; auto.
+rewrite_env (nil ++ G1) in H5. rewrite_env (nil ++ G1). eauto using zip_remove_E.
+rewrite_env (env_map (tsubst_typ (typ_var_f a) b) nil ++ a ~ Eq t ++ G1).
+apply wfterm_instantiate.
+replace (typ_arrow (tsubst_typ (typ_var_f a) b t2) (tsubst_typ (typ_var_f a) b τ))
+with (tsubst_typ (typ_var_f a) b (typ_arrow t2 τ)) by reflexivity.
+apply wfterm_renameU; auto.
+apply wfterm_weakening; auto. constructor; auto.
+  eapply wfterm_wfenv; eauto. apply pure_U.
+  intros. unfold ftv_env in H6; simpl in H6. intro. clear Fr. fsetdec.
+apply wfenv_wftyp_Eq3 with (x := a).
+apply wfenv_zip21 with (Γ₂ := a ~ Eq t ++ G3 ++ G0) (Γ₃ := a ~ Eq t ++ x ++ x0).
+constructor; auto. rewrite_env (nil ++ G1).
+  rewrite_env (nil ++ G1) in H5. eapply zip_remove_E; eauto.
+intros. apply ftv_env_binds in H6. destruct H6 as [? [? [? [? | ?]]]].
+eapply wfterm_T_not_E; eauto.
+eapply wfterm_Eq_not_E; eauto.
+eapply wfterm_wfenv; eauto.
+intros. eapply val_pure; eauto.
+Case "sigma_pairL". inversion H7; subst.
+assert (binds b U G2) by eauto with fzip.
+assert (binds b E Γ) by eauto with fzip.
+apply binds_decomp in H3. destruct H3 as [? [? ?]]; subst.
+apply binds_decomp in H4. destruct H4 as [? [? ?]]; subst.
+pick fresh a and apply wfterm_sigma.
+assert (uniq (x1 ++ [(b, E)] ++ x2)) by eauto with lngen. solve_uniq.
+assert (lc_typ t).
+  assert (lc_term (term_sigma (typ_var_f b) t e1))
+    by eauto with lngen. inversion H3; subst; auto.
+replace (open_term_wrt_typ (term_pair e1 e2') (typ_var_f a)) with
+  (term_pair (open_term_wrt_typ e1 (typ_var_f a)) (open_term_wrt_typ e2' (typ_var_f a))) by reflexivity.
+simpl. simpl_env.
+rewrite H2; auto.
+apply wfterm_pair with
+  (G1 := a ~ Eq t ++ G3 ++ G0)
+  (G2 := a ~ Eq t ++ x ++ x0).
+constructor; eauto using zip_remove_EU.
+apply H12; auto.
+rewrite_env (env_map (tsubst_typ (typ_var_f a) b) nil ++ a ~ Eq t ++ x ++ x0).
+apply wfterm_instantiate.
+apply wfterm_renameU; auto.
+apply wfterm_lowerU; auto.
+intro H4. apply ftv_env_binds in H4. destruct H4 as [? [? [? [? | ?]]]].
+  assert (binds x3 (T x4) (x ++ b ~ U ++ x0)) by auto.
+    eapply zip_binds_T23 in H8; eauto. eapply zip_binds_T31 in H8; eauto.
+    assert (ftv_typ x4 [<=] dom (G3 ++ G0)). apply wftyp_ftv.
+    apply wfenv_wftyp_T2 with (x := x3); auto.
+    apply wfenv_strip with (Γ' := a ~ Eq t). eapply wfterm_wfenv. eauto with fzip.
+    analyze_binds H8.
+    clear Fr. assert (b ∉ ftv_typ x4) by fsetdec. contradiction.
+  assert (binds x3 (Eq x4) (x ++ b ~ U ++ x0)) by auto.
+    eapply zip_binds_Eq23 in H8; eauto. eapply zip_binds_Eq31 in H8; eauto.
+    assert (ftv_typ x4 [<=] dom (G3 ++ G0)). apply wftyp_ftv.
+    apply wfenv_wftyp_Eq2 with (x := x3); auto.
+    apply wfenv_strip with (Γ' := a ~ Eq t). eapply wfterm_wfenv; eauto.
+    analyze_binds H8.
+    clear Fr. assert (b ∉ ftv_typ x4) by fsetdec. contradiction.
+apply zip_remove_EU in H5. eapply wftyp_zip12; eauto. apply wfenv_wftyp_Eq3 with (x := a).
+eapply wfterm_wfenv; eauto.
+eapply wfenv_zip12; eauto. apply wfenv_strip with (Γ' := a ~ Eq t).
+eapply wfterm_wfenv; eauto.
+apply zip_remove_EU in H5.
+intros.
+assert (a0 ∈ dom (G3 ++ G0)).
+assert (ftv_typ t [<=] dom (G3 ++ G0)). apply wftyp_ftv. apply wfenv_wftyp_Eq3 with (x := a). eapply wfterm_wfenv; eauto. auto.
+apply binds_In_inv in H6. destruct H6. destruct x3.
+eapply zip_binds_T12 in H6; eauto. intro.
+  assert (E = T t0). eapply binds_unique; eauto with lngen. congruence.
+eapply zip_binds_U12 in H6; eauto. intro.
+  assert (@E typ = U). eapply binds_unique; eauto with lngen. congruence.
+eapply zip_binds_E12 in H6; eauto. intro.
+  assert (@E typ = U). eapply binds_unique; eauto with lngen. congruence.
+eapply zip_binds_Eq12 in H6; eauto. intro.
+  assert (@E typ = Eq t0). eapply binds_unique; eauto with lngen. congruence.
+Case "sigma_pairR". inversion H9; subst.
+assert (binds b E Γ) by eauto with fzip.
+assert (b ∉ dom G1).
+  eapply zip_binds_E3_inv in H3; eauto. destruct H3 as [[? ?] | [? ?]]; auto.
+  analyze_binds_uniq H4; eauto with lngen.
+apply binds_decomp in H3. destruct H3 as [? [? ?]]; subst.
+pick fresh a and apply wfterm_sigma.
+assert (uniq (x ++ [(b, E)] ++ x0)) by eauto with lngen. solve_uniq.
+assert (lc_typ t).
+  assert (lc_term (term_sigma (typ_var_f b) t e2))
+    by eauto with lngen. inversion H3; subst; auto.
+replace (open_term_wrt_typ (term_pair e1' e2) (typ_var_f a)) with
+  (term_pair (open_term_wrt_typ e1' (typ_var_f a)) (open_term_wrt_typ e2 (typ_var_f a))) by reflexivity.
+simpl. simpl_env.
+rewrite H2; auto.
+apply wfterm_pair with
+  (G1 := a ~ Eq t ++ G1)
+  (G2 := a ~ Eq t ++ G3 ++ G0); eauto.
+constructor; auto.
+rewrite_env (nil ++ G1) in H5. rewrite_env (nil ++ G1). eauto using zip_remove_E.
+rewrite_env (env_map (tsubst_typ (typ_var_f a) b) nil ++ a ~ Eq t ++ G1).
+apply wfterm_instantiate.
+apply wfterm_renameU; auto.
+apply wfterm_weakening; auto. constructor; auto.
+  eapply wfterm_wfenv; eauto. apply pure_U.
+  intros. unfold ftv_env in H6; simpl in H6. intro. clear Fr. fsetdec.
+apply wfenv_wftyp_Eq3 with (x := a).
+apply wfenv_zip21 with (Γ₂ := a ~ Eq t ++ G3 ++ G0) (Γ₃ := a ~ Eq t ++ x ++ x0).
+constructor; auto. rewrite_env (nil ++ G1).
+  rewrite_env (nil ++ G1) in H5. eapply zip_remove_E; eauto.
+intros. apply ftv_env_binds in H6. destruct H6 as [? [? [? [? | ?]]]].
+eapply wfterm_T_not_E; eauto.
+eapply wfterm_Eq_not_E; eauto.
+eapply wfterm_wfenv; eauto.
+intros. eapply val_pure; eauto.
+Case "sigma_fst". inversion H3; subst.
+pick fresh a and apply wfterm_sigma; auto.
+replace (open_term_wrt_typ (term_fst e) (typ_var_f a)) with
+(term_fst (open_term_wrt_typ e (typ_var_f a))) by reflexivity.
+apply wfterm_fst with (t2 := tsubst_typ (typ_var_f a) b t2).
+replace (typ_prod (tsubst_typ (typ_var_f a) b τ) (tsubst_typ (typ_var_f a) b t2))
+with (tsubst_typ (typ_var_f a) b (typ_prod τ t2)) by reflexivity.
+auto.
+Case "sigma_snd". inversion H3; subst.
+pick fresh a and apply wfterm_sigma; auto.
+replace (open_term_wrt_typ (term_snd e) (typ_var_f a)) with
+(term_snd (open_term_wrt_typ e (typ_var_f a))) by reflexivity.
+apply wfterm_snd with (t1 := tsubst_typ (typ_var_f a) b t1).
+replace (typ_prod (tsubst_typ (typ_var_f a) b t1) (tsubst_typ (typ_var_f a) b τ))
+with (tsubst_typ (typ_var_f a) b (typ_prod t1 τ)) by reflexivity.
+auto.
+Case "sigma_inst". inversion H8; subst.
+pick fresh a and apply wfterm_sigma; auto.
+replace (open_term_wrt_typ (term_inst e t'') (typ_var_f a))
+with (term_inst (open_term_wrt_typ e (typ_var_f a)) (open_typ_wrt_typ t'' (typ_var_f a)))
+by reflexivity.
+rewrite tsubst_typ_open_typ_wrt_typ; auto.
+rewrite H2; auto.
+apply wfterm_inst.
+rewrite_env (env_map (tsubst_typ (typ_var_f a) b) nil ++ a ~ Eq t ++ G2 ++ G1).
+apply wftyp_instantiate. apply wftyp_renameU; auto.
+apply wftyp_EU. apply wftyp_lowerE; auto.
+intro. apply ftv_env_binds in H3. destruct H3 as [? [? [? [? | ?]]]].
+  assert (ftv_typ x0 [<=] dom (G2 ++ G1)).
+    apply wftyp_ftv. apply wfenv_wftyp_T2 with (x := x); auto.
+    apply wfenv_strip with (Γ' := a ~ Eq t). eapply wfterm_wfenv; eauto.
+    clear Fr. fsetdec.
+  assert (ftv_typ x0 [<=] dom (G2 ++ G1)).
+    apply wftyp_ftv. apply wfenv_wftyp_Eq2 with (x := x); auto.
+    apply wfenv_strip with (Γ' := a ~ Eq t). eapply wfterm_wfenv; eauto.
+    clear Fr. fsetdec.
+apply wfenv_wftyp_Eq3 with (x := a). eapply wfterm_wfenv; eauto.
+apply H11; auto.
 Case "sigma_open".
+assert (uniq (G2 ++ c ~ E ++ G1)) by eauto with lngen.
+inversion H6; subst.
+assert (binds b E (G2 ++ G1)). rewrite <- H2. auto.
+analyze_binds H3; apply binds_decomp in BindsTac;
+destruct BindsTac as [? [? ?]]; subst.
+SCase "b binds in G2".
+simpl_env in H2.
+symmetry in H2. apply uniq_app_inv in H2. destruct H2; subst. simpl_env.
+pick fresh a and apply wfterm_sigma; auto.
+replace (open_term_wrt_typ (term_open (typ_var_f c) e) (typ_var_f a))
+with (term_open (typ_var_f c) (open_term_wrt_typ e (typ_var_f a)))
+by reflexivity.
+rewrite tsubst_typ_open_typ_wrt_typ; auto.
+simpl. destruct (c == b); subst.
+SSSCase "c = b (absurd)". elimtype False. clear Fr.
+  simpl_env in *. fsetdec.
+SSSCase "c ≠ b".
+rewrite_env (([(a, Eq t)] ++ G3 ++ x0) ++ [(c, E)] ++ G1).
+apply wfterm_open. solve_uniq. simpl_env; apply H10; auto.
+solve_uniq.
+SCase "b binds in G1".
+simpl_env in H2. rewrite_env ((G2 ++ x) ++ [(b, E)] ++ x0) in H2.
+symmetry in H2. apply uniq_app_inv in H2. destruct H2; subst. simpl_env.
+rewrite_env ((G2 ++ [(c, E)] ++ x) ++ [(b, E)] ++ G0).
+pick fresh a and apply wfterm_sigma; auto.
+replace (open_term_wrt_typ (term_open (typ_var_f c) e) (typ_var_f a))
+with (term_open (typ_var_f c) (open_term_wrt_typ e (typ_var_f a)))
+by reflexivity.
+rewrite tsubst_typ_open_typ_wrt_typ; auto.
+simpl. destruct (c == b); subst.
+SSSCase "c = b (absurd)". elimtype False. clear Fr.
+  simpl_env in *. fsetdec.
+SSSCase "c ≠ b".
+rewrite_env (([(a, Eq t)] ++ G2) ++ [(c, E)] ++ x ++ G0).
+apply wfterm_open. solve_uniq.
+rewrite_env ([(a, Eq t)] ++ (G2 ++ x) ++ G0). apply H10; auto.
+solve_uniq.
 Case "sigma_sigma".
 
-
 ICI
+
+
 
 Theorem subject_reduction : forall Γ e e' τ,
   wfterm Γ e τ → e ⇝ e' → wfterm Γ e' τ.
